314e5d2d444c44f6a91118dfec1a0a46d9c33f0d
FinallySays <shiwplee@gmail.com>
2021-01-23 11:22:34 +0800

    V1

---
+++ src/main/java/com/mashibing/tank/Main.java
@@ -0,0 +1,19 @@
+package com.mashibing.tank;
+
+
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * @Author : shiwp
+ * @Date : 2021/1/23 9:42 上午
+ * @Description : com.mashibing.tank
+ * @Version : 1.0
+ */
+public class Main {
+    public static void main(String[] args) throws InterruptedException {
+        TankFrame tankFrame = new TankFrame();
+
+    }
+}
---
+++ src/main/java/com/mashibing/tank/TankFrame.java
@@ -0,0 +1,90 @@
+package com.mashibing.tank;
+
+import java.awt.*;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+
+/**
+ * @Author : shiwp
+ * @Date : 2021/1/23 9:57 上午
+ * @Description : com.mashibing.tank
+ * @Version : 1.0
+ */
+public class TankFrame extends Frame {
+
+    int x = 200, y = 200;
+
+    public TankFrame() {
+        setSize(800, 600);
+        setResizable(false);
+        setTitle("tank war");
+        setVisible(true);
+
+        addKeyListener(new MykeyListener());
+
+        addWindowListener(new WindowAdapter() {
+            @Override
+            public void windowClosing(WindowEvent e) {
+                System.exit(0);
+            }
+        });
+    }
+
+    @Override
+    public void paint(Graphics g) {
+        g.fillRect(x, y, 50, 50);
+//        x += 10;
+    }
+
+    class MykeyListener extends KeyAdapter {
+
+        boolean bl = false;
+        boolean br = false;
+        boolean bu = false;
+        boolean bd = false;
+
+        @Override
+        public void keyPressed(KeyEvent e) {
+            int keyCode = e.getKeyCode();
+            switch (keyCode) {
+                case KeyEvent.VK_LEFT:
+                    bl = true;
+                    break;
+                case KeyEvent.VK_RIGHT:
+                    br = true;
+                    break;
+                case KeyEvent.VK_DOWN:
+                    bd = true;
+                    break;
+                case KeyEvent.VK_UP:
+                    bu = true;
+                    break;
+            }
+            x = x + (bl ? -10 : 0) + (br ? 10 : 0);
+            y = y + (bu ? -10 : 0) + (bd ? 10 : 0);
+            repaint();
+
+        }
+
+        @Override
+        public void keyReleased(KeyEvent e) {
+            int keyCode = e.getKeyCode();
+            switch (keyCode) {
+                case KeyEvent.VK_LEFT:
+                    bl = false;
+                    break;
+                case KeyEvent.VK_RIGHT:
+                    br = false;
+                    break;
+                case KeyEvent.VK_DOWN:
+                    bd = false;
+                    break;
+                case KeyEvent.VK_UP:
+                    bu = false;
+                    break;
+            }
+        }
+    }
+}
